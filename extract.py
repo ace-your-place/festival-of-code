813127502215e0d57db73b6dfbaee2917e5caf3e
Ben <benjames1999@hotmail.co.uk>
2014-07-29 15:54:06 +0100

    Added crime data

--- extract.py
+++ extract.py
@@ -1,24 +1,11 @@
-import csv
 import pandas
+import csv
+from glob import glob
 
-#with open("data/crime/2014-05-avon-and-somerset-street.csv", "r") as file:
-#	file.readline()
-#	print(file.readline())
+files = glob("data/crime/*.csv")
 
-Crime_Data = list()
+#print files
 
-cr = csv.reader(open("data/crime/2014-05-avon-and-somerset-street.csv","rb"))
-for row in cr:    
-	data = pandas.read_csv("data/crime/2014-05-avon-and-somerset-street.csv", usecols=[4,5])
-	Crime_Data.append(data)
-
-Crime_Data.append("test")
-
-print(Crime_Data)
-	
-
-
-	# [
-	# (a, b),
-	# ]
-
+for file in files:
+    data = pandas.read_csv(file, usecols=[4,5])
+    data.to_csv("data/crime_data.csv", index=False, cols=['Longitude', 'Latitude'], mode='a')
