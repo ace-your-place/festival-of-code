c3bea793fa97a342cd1b0a9f41afbff0989e342c
Ben <benjames1999@hotmail.co.uk>
2014-07-29 13:59:36 +0100

    extract.py for John

--- extract.py
+++ extract.py
@@ -1,2 +1,24 @@
-#!usr/bin/python
+import csv
+import pandas
+
+#with open("data/crime/2014-05-avon-and-somerset-street.csv", "r") as file:
+#	file.readline()
+#	print(file.readline())
+
+Crime_Data = list()
+
+cr = csv.reader(open("data/crime/2014-05-avon-and-somerset-street.csv","rb"))
+for row in cr:    
+	data = pandas.read_csv("data/crime/2014-05-avon-and-somerset-street.csv", usecols=[4,5])
+	Crime_Data.append(data)
+
+Crime_Data.append("test")
+
+print Crime_Data
+	
+
+
+	# [
+	# (a, b),
+	# ]
 
