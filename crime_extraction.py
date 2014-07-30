564784540e4d0da609548c9d4f6fd2bd2bebfbed
Ben <benjames1999@hotmail.co.uk>
2014-07-30 16:35:00 +0100

    finalise crime data sorting

--- crime_extraction.py
+++ crime_extraction.py
@@ -9,3 +9,5 @@ files = glob("data/crime/*.csv")
 for file in files:
     data = pandas.read_csv(file, usecols=[7])
     data.to_csv("data/crime_data.csv", index=False, mode='a')
+
+#MUST SORT BLANK CSV LINES IN OUTPUT CSV
