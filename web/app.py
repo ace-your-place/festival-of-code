c2f78f57958f0c799ce943f64d944eb2b9560816
Finnian Anderson <finnian@fxapi.co.uk>
2014-07-30 16:39:51 +0100

    milestone for mapping icons

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+loc = []
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -13,7 +13,8 @@ def cakes():
 def map():
 	lat = request.args.get('latitude')
 	lng = request.args.get('longitude')
-	return render_template('map.html', latitude = lat, longitude = lng)
+	l = request.args.get('location')
+	return render_template('map.html', latitude = lat, longitude = lng, location = l)
 
 if __name__ == '__main__':
 	app.run(debug=True)
