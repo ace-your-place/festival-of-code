2b44ba51beedd9ab68ba6454386c1a1f8e1b003a
Ben <benjames1999@hotmail.co.uk>
2014-07-30 09:39:39 +0100

    added noise averages

--- noise_average.py
+++ noise_average.py
@@ -5,13 +5,14 @@ ofile  = open('data/noise_average.csv', "wb")
 writer = csv.writer(ofile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
 lines = 0
 
-with open('data/noise/r2_strategic_noise_mapping.csv', 'rb') as f:
+with open('data/noise.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         lines += 1
         #print lines
         if lines != 1:
-            average = row[]
+            average = (float(row[1]) + float(row[2]))/2
+            #print average
             data = [row[0], average]
-        #print data
+            #print data
             writer.writerow(data)
