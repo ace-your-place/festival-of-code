2f3e0a4891bad926330062ebb8816b3c331e35cd
Finnian Anderson <finnian@fxapi.co.uk>
2014-07-31 14:11:11 +0100

    Fix import

--- web/app.py
+++ web/app.py
@@ -12,7 +12,7 @@ def form():
 	crime_data = 0#equest.form.crime
 	education_data = 1#request.form.education
 	lat_long = getLocation(crime=crime_data,education=education_data)
-	return render_template('map', latitude = lat_long[0], longitude = lat_long[1])
+	return render_template('map.html', latitude = lat_long[0], longitude = lat_long[1])
 
 @app.route('/map', methods=['GET'])
 def map():
