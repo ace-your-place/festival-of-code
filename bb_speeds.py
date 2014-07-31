98b2eefbad283731768aa2f7ca072c50c6a6ed79
Johare24 <jptohare@googlemail.com>
2014-07-31 13:49:22 +0100

    add comment bbspeeds

--- bb_speeds.py
+++ bb_speeds.py
@@ -3,3 +3,5 @@ from pandas import DataFrame, read_csv
 data = read_csv('data/broadband/ofcom-uk-broadband-speed-data-2013.csv', usecols=[0,2])
 
 data.to_csv('data/bbdata.csv', index=False)
+
+#Works
