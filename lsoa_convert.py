c08f0361e2e12c258a1df0a6221797e6b793afbb
Finnian Anderson <finnian@fxapi.co.uk>
2014-07-31 12:24:59 +0100

    Fixed empty csv

--- lsoa_convert.py
+++ lsoa_convert.py
@@ -9,10 +9,11 @@ lines = 0
 with open('data/lsoas/ONSPD_FEB_2012_UK_uk_datazone_centroids.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
+	print row
         lines += 1
         #print lines
-        if lines <= 2:
-            data = row[0], cv.ENtoLL84(row[1], row[2])[0], cv.ENtoLL84(row[1], row[2])[1]
+        if lines > 2:
+            data = [row[0], cv.ENtoLL84(row[1], row[2])[0], cv.ENtoLL84(row[1], row[2])[1]]
 
-            #writer.writerow(data)
+            writer.writerow(data)
             print data
