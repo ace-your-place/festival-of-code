c6ee7ecb5cd2a9aa49e27ba4ebfb4a0a4aef835d
Finnian Anderson <finnian@fxapi.co.uk>
2014-07-31 17:42:22 +0100

    Fix multiple mapping pins

--- web/app.py
+++ web/app.py
@@ -10,12 +10,8 @@ def index():
 @app.route('/form', methods=['POST','GET'])
 def form():
 	if request.method == 'POST':
-#		print request.form['crime']
-#		print request.form['education']
-#		crime_data = request.form['crime']
-#		education_data = request.form('education')
-		lat_long = getLocation(crime = request.form['crime'], education = request.form['education'])
-		return render_template('map.html', location = lat_long)
+		lat_longs = getLocation(crime = request.form['crime'], education = request.form['education'])
+		return render_template('map.html', locations = lat_longs)
 
 @app.route('/map', methods=['GET'])
 def map():
