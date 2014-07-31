51cc64882bcee03bda840527ae487627cda3c6bb
Ben <benjames1999@hotmail.co.uk>
2014-07-31 14:05:59 +0100

    add let/long return

--- web/getLocation.py
+++ web/getLocation.py
@@ -1,9 +1,11 @@
 import random
 
-latlong = []
+latlong = [[1.7524139512, 52.4863520771], [1.7136396631, 52.4626442228], [1.5613339044, 52.7500014395], [1.3810863335, 51.2198327852]]
 
-random.randint
 
-def getLocation(crime, education)
+def getLocation(crime, education):
+    randomCase = random.randint(0,3)
 
-    return latlong
+    return latlong[randomCase]
+
+#print getLocation(1,3)
