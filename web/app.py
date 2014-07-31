94347f53c588e6ffd3b6129c7cb2c9b65f1d3bdb
Finnian Anderson <finnian@fxapi.co.uk>
2014-07-31 13:54:47 +0100

    Fixed incorrect imports

--- web/app.py
+++ web/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
+#from getLocation import getLocation
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/form', methods=['POST'])
+@app.route('/form', methods=['GET'])
 def form():
 	crime_data = request.form.crime
 	education_data = request.form.education
-	lat_long = process.getLocation(crime=crime_data,education=education_data)
+	lat_long = getLocation(crime=crime_data,education=education_data)
 	return render_template('map', latitude = lat_long[0], longitude = lat_long[1])
 
 @app.route('/map', methods=['GET'])
