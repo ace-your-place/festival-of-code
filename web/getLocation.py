ad3cd4de425abbd1ad33c223e5cc1182c803bd9c
Finnian Anderson <finnian@fxapi.co.uk>
2014-08-01 10:30:13 +0100

    Fix map bug

--- web/getLocation.py
+++ web/getLocation.py
@@ -18,7 +18,7 @@ latlongs = [
     [53.3694336227, -2.7122767367],
     [52.5262270486, -1.9966547854],
     [55.7574785674, -2.0019467746],
-    [54.1932611002  -3.0791961468],
+    [54.1932611002,  -3.0791961468],
     [56.1224543752, -3.1427113521],
     [56.4633781494, -3.1701595478],
     [57.814049437, -4.0593344221],
@@ -47,9 +47,12 @@ print lsoas
 print crimes
 print schools
 
-getLocation(crime=5, education=4)
+#getLocation(crime=5, education=4)
 
 def getLocationRand(crime = 0, education = 0):
-    random_latlong_1 = choice(latlongs)
-    random_latlong_2 = choice(latlongs)
-    return [random_latlong_1, random_latlong_2]
+	random_latlong_1 = choice(latlongs)
+	random_latlong_2 = choice(latlongs)
+	random_latlong_3 = choice(latlongs)
+	random_latlong_4 = choice(latlongs)
+	random_latlong_5 = choice(latlongs)
+	return [random_latlong_1, random_latlong_2]
