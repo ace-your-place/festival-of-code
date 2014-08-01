af0ac3824b01dcdeb47b0eaeee2b082857ef70fa
Finnian Anderson <finnian@fxapi.co.uk>
2014-08-01 09:24:07 +0100

    Fixed indentation error

--- web/getLocation.py
+++ web/getLocation.py
@@ -1,10 +1,13 @@
 from random import choice
 import csv
 import pandas
-import deque
+from collections import deque
 
-crimes = deque(max=20)
-schools = deque(max=20)
+crimes = deque(maxlen=20)
+schools = deque(maxlen=20)
+
+lastSaved_crime_high = 0
+lastSaved_school_high = 0
 
 latlongs = [
 	[52.4863520771, 1.7524139512],
@@ -13,11 +16,23 @@ latlongs = [
     [53.3694336227, -2.7122767367]
 ]
 
-
+line = 0
 
 def getLocation(crime = 0, education = 0):
-	for row in table:
-		
+	with open('../data/merge.csv', 'rb') as table:
+		reader = csv.reader(table)
+		for row in reader:
+			if line != 1:
+				if (row[1] >= range(crime - 2, crime, crime + 2)) and (row[3] >= range(education - 2, education, education + 2)):
+					print row[1]
+					print row[3]
+					lastSaved_crime_high = row[1]
+					lastSaved_school_high = row[3]
+
+	print("Highest school ratings: ", lastSaved_school_high)
+	print("Highest crimes: ", lastSaved_crime_high)
+
+#getLocation(crime=5, education=10)
 
 def getLocationRand(crime = 0, education = 0):
     random_latlong_1 = choice(latlongs)
